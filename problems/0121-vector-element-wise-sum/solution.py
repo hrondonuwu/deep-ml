61545974ee39a037090621e90004db17da9e8198
hrondonuwu <154196995+hrondonuwu@users.noreply.github.com>
2026-09-20 18:58:18 +0000

    Update: Vector Element-wise Sum

--- problems/0121-vector-element-wise-sum/solution.py
+++ problems/0121-vector-element-wise-sum/solution.py
@@ -1,9 +1,6 @@
+import numpy as np
 def vector_sum(a: list[int|float], b: list[int|float]) -> list[int|float]:
-	sum = []
 	if len(a) != len(b):
 		return -1
-	
-	for i in range (len(a)):
-		sum.append(a[i] + b[i])
-	
-	return sum
+
+	return (np.array(a) + np.array(b)).tolist()
